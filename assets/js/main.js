760f121191e69ec56ff7c8723516a836ee3a22a9
shaharearalamin <shaharearalamin@gmail.com>
2025-01-10 23:29:53 +0600

    fixing testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,4 +112,26 @@
       headersticky.classList.remove("headersticky");
     }
   };
+
+  // testimonials img perspective
+  const container = document.getElementById("imageContainer");
+  const image = document.getElementById("perspectiveImage");
+
+  container.addEventListener("mousemove", (event) => {
+    const rect = container.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((centerY - y) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    image.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  container.addEventListener("mouseleave", () => {
+    image.style.transform = "rotateX(0deg) rotateY(0deg)";
+  });
 })(jQuery);
